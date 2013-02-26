023086ade2b5b5a782f293b1f8e2ef80040c701c
root <root@mtkswgpn14.(none)>
2013-02-26 09:04:58 +0800

    display python info

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import os
+from pyinfo import pyinfo
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
+def pyinfopage():
+    return pyinfo()
+
+@app.route('/hello')
 def hello():
     return 'Hello World!'
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 8080.
